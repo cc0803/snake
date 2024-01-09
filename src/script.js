8473ac012e057ca86702d1d9ba49a017e535d067
cc0803 <simongeldbach@gmail.com>
2024-01-09 18:21:25 +0100

    Add javascript for making snakeFields green and change scss sizing

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
 import "./styles.scss";
+
+let playingField = document.querySelectorAll(".field");
+
+let direction;
+
+let foodField;
+
+let snake = [5, 6, 7, 8, 24];
+
+snake.forEach((element) => {
+	playingField[element].classList.add("green");
+});
