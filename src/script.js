7b7b106aeedbc9a926daed80f6b9b1cdf6c030a2
cc0803 <simongeldbach@gmail.com>
2024-01-11 16:55:12 +0100

    Create function for letting the snake move downwards

--- src/script.js
+++ src/script.js
@@ -6,8 +6,25 @@ let direction;
 
 let foodField;
 
-let snake = [5, 6, 7, 8, 24];
+let snake = [24, 8, 7, 6, 5];
 
-snake.forEach((element) => {
-	playingField[element].classList.add("green");
-});
+function displaySnake() {
+	snake.forEach((element) => {
+		playingField[element].classList.add("green");
+	});
+}
+
+function removeSnakeTail(field) {
+	playingField[field].classList.remove("green");
+}
+
+displaySnake();
+
+// let interval = setInterval(moveSnake, 1000);
+
+function moveSnake() {
+	snake.unshift(snake[0] + 16);
+	console.log(snake);
+	removeSnakeTail(snake.pop());
+	displaySnake();
+}
